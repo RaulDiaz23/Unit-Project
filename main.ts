60db68af925b2012885b6dce27b7df3c60780dce
BeckScot <124732042+BeckScot@users.noreply.github.com>
2023-02-08 14:03:41 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
+function NameSelection (P1name: string, P2name: string) {
+    p1name = P1name
+    p2name = P2name
+}
+let p2name = ""
+let p1name = ""
 scene.setBackgroundImage(assets.image`Space1`)
+NameSelection(game.askForString("Player1 Name"), game.askForString("Player 2 Name"))
